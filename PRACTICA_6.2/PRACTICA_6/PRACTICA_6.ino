c57efafa2c0e19e80bdeb44665a983e2c740e124
Civan98 <52634607+Civan98@users.noreply.github.com>
2019-07-07 16:24:38 -0500

    lol
    davsdv

---
+++ PRACTICA_6.2/PRACTICA_6/PRACTICA_6.ino
@@ -0,0 +1,21 @@
+ int Tinaco_lleno = 5;
+ int Cisterna = 6;
+ int Bomba = 7; 
+
+void setup() {
+   pinMode (Tinaco_lleno,INPUT);
+   pinMode (Cisterna,INPUT);
+   pinMode (Bomba,OUTPUT);
+}
+
+void loop() {
+   if(digitalRead(Cisterna)==HIGH){
+     if(digitalRead(Tinaco_lleno)==LOW){
+        digitalWrite (Bomba,HIGH);
+        }
+      else if (digitalRead(Tinaco_lleno)==HIGH){
+        digitalWrite (Bomba,LOW);
+      }
+   }else digitalWrite (Bomba,LOW);
+
+}
